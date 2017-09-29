114fa1cfc87a2892d14c1e00f02663bc75bcdab7
JhollSkill <32115855+JhollSkill@users.noreply.github.com>
2017-09-29 20:51:17 -0300

    Add files via upload

---
+++ Codigos/UsarFormularioEventos/src/usarformularioeventos/FormularioAlunoNovo.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package usarformularioeventos;
+
+import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+/**
+ *
+ * @author 010617041
+ */
+public class FormularioAlunoNovo extends Frame implements ActionListener {
+    
+    protected Dimension dQuadro, dRotulo, dCaixaTexto, dBotao;
+    protected Button bValidar, bSair;
+    protected TextField tfAluno;
+    protected Label lAluno;
+        
+        Aluno alunoUm = new Aluno("Eliane");
+        Aluno alunoDois = new Aluno("Jonatas");
+        Aluno alunoTres = new Aluno("Rodrigo");
+        
+        public FormularioAlunoNovo() {
+            
+            Dimension dQuadro = new Dimension(290,200);
+            Dimension dRotulo = new Dimension(45,25);
+            Dimension dCaixaTexto = new Dimension(180,25);
+            Dimension dBotao = new Dimension(100,25);
+            
+            setTitle("Novo Validar Aluno");
+            setResizable(false);
+            setSize(dQuadro);
+            setLocation(650,350);
+            setLayout(null);
+            
+            lAluno = new Label("Aluno");
+            lAluno.setSize(dRotulo);
+            lAluno.setLocation(25,50);
+            
+            tfAluno = new TextField();
+            tfAluno.setSize(dCaixaTexto);
+            tfAluno.setLocation(80,50);
+            
+            bValidar = new Button("validar aluno");
+            bValidar.setSize(dBotao);
+            bValidar.setLocation(25,150);
+            bValidar.addActionListener(this);
+            
+            bSair = new Button("Sair");
+            bSair.setSize(dBotao);
+            bSair.setLocation(130,150);
+            bSair.addActionListener(this);
+            
+            add(lAluno);
+            add(tfAluno);
+            add(bValidar);
+            add(bSair);
+        }
+    public void actionPerformed(ActionEvent evento) {
+        
+        if(evento.getSource() == bValidar) {
+            alunoUm.validarAluno(tfAluno.getText());
+        }
+        if (evento.getSource() == bSair) {
+            System.out.println("Fim do Aplicativo. Obrigado!");
+                System.exit(0);
+        }
+    }
+}
